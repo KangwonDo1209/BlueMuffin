a843675cf3979c5ecba2672633dac1da299a62c7
1209 <ehtjd1209@gmail.com>
2023-10-12 18:16:57 +0900

    23_10_11

--- Assets/Scripts/UI/ShowFPS.cs
+++ Assets/Scripts/UI/ShowFPS.cs
@@ -7,21 +7,23 @@ using UnityEngine.UI;
 public class ShowFPS : MonoBehaviour
 {
 	public TMP_Text FPSText;
-	public string defaultString = "FPS : ";
+	public Coroutine CalculateFPS_Coroutine;
+
+	public string defaultFpsString = "FPS : ";
 	private string fpsString;
 	private int fps;
 
 	private void Start()
 	{
-		StartCoroutine(CalculateFPS());
+		CalculateFPS_Coroutine = StartCoroutine(CalculateFPS());
 	}
 
 	IEnumerator CalculateFPS()
 	{
-		while(true)
+		while (true)
 		{
 			fps = (int)(1.0f / Time.deltaTime);
-			fpsString = defaultString + fps.ToString("000");
+			fpsString = defaultFpsString + fps.ToString("000");
 			FPSText.text = fpsString;
 			yield return new WaitForSeconds(0.5f);
 		}
